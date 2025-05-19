95fab39d85d247f4eebb6baa14a833c59c7a26f1
foysal777 <foysal.cse11@gmail.com>
2025-05-19 21:46:28 +0600

    Customized Admin.py

--- admission/admin.py
+++ admission/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Student
-# Register your models here.
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 
+        'name', 
+        'course_applied', 
+        'father_name', 
+        'date_of_birth', 
+        'religion', 
+        'nationality',
+        'mobile_number', 
+        'email'
+    )
+    search_fields = ('name', 'father_name', 'email', 'mobile_number')
+    list_filter = ('religion', 'nationality', 'course_applied')
+    ordering = ('-id',)  # latest entries first
+
+admin.site.register(Student, StudentAdmin)
