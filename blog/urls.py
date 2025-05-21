103fe02de204ced804a52c7c33fecbc3eb1ef0ae
foysal777 <foysal.cse11@gmail.com>
2025-05-21 07:06:54 +0600

    add urls.py project api

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import PostListCreateAPIView ,TotalPostCountAPIView , delete_post, GalleryView
+from .views import PostListCreateAPIView ,ProjectAPIView,TotalPostCountAPIView , delete_post, GalleryView
 
 urlpatterns = [
     path('total-blog-count/', TotalPostCountAPIView.as_view(), name='total-blog'),
     path('post/', PostListCreateAPIView.as_view(), name='blog'),
     path('post/delete/<int:post_id>/', delete_post, name='delete_post'),
     path('gallery/', GalleryView.as_view(), name='Galleryimages'),
+    path('api/projects/', ProjectAPIView.as_view(), name='project-list-create'),
 
   
 ]
