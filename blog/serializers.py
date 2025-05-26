f37c887fb4212af2ea181d90339d9d7e73f7ff89
foysal777 <foysal.cse11@gmail.com>
2025-05-26 09:51:14 +0600

    solve  type error of serializers.py

--- blog/serializers.py
+++ blog/serializers.py
@@ -16,5 +16,5 @@ class GallerySerializer(serializers.ModelSerializer):
 class ProjectSerializers(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = '__all_'
+        fields = '__all__'
         
