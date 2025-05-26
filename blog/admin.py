b7d1b090a9356ed61ba544b1c84937864e45eb78
foysal777 <foysal.cse11@gmail.com>
2025-05-26 09:44:31 +0600

    add blog apps project in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post,Gallery
+from .models import Post,Gallery, Project
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Gallery)
+admin.site.register(Project)
